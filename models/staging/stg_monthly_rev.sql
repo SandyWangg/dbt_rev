755e9f5fc31663f03c9c5f38a7ddc2f700b838ba
Sandy Wang <sandywang@SandydeMacBook-Pro.local>
2023-07-05 12:13:39 +0800

    [fix] add is_incremental

--- models/staging/stg_monthly_rev.sql
+++ models/staging/stg_monthly_rev.sql
@@ -1,5 +1,7 @@
-{{config({"materialized" : "incremental"}
-)}}
+{{ config( 
+        materialized = "incremental", 
+        unique_key = "Date"
+) }}
 
 with orders_view as (
 
@@ -17,11 +19,32 @@ payments_view as (
 
 monthly_rev as (
 
-    SELECT strftime('%Y-%m', order_date) AS "Date", sum(amount) as "Monthly Revenue"
+    SELECT 
+        strftime('%Y-%m', order_date) AS "Date", 
+        strftime('%Y-%m', order_date) AS "event_time",
+        sum(amount) AS "Monthly Revenue"
+    
     FROM  orders_view join payments_view on orders_view.order_id = payments_view.order_id
     AND "status" NOT IN ('return_pending', 'returned')
+
+    WHERE "event_time" < strftime('%Y-%m', datetime('now'))
+    
     GROUP BY Date
 
 )
 
 select * from monthly_rev
+
+
+{% if is_incremental() %}
+--   this filter will only be applied on an incremental run
+
+  WHERE event_time > (select max(event_time) from {{ this }}) 
+  AND strftime('%Y-%m', event_time) < strftime('%Y-%m', datetime('now'))
+
+{% else %}
+  -- this will run on a full-refresh
+
+  WHERE event_time < strftime('%Y-%m', datetime('now'))
+
+{% endif %}
